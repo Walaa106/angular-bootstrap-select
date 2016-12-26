caaed9f9dea1629c1c90619f66ea8cdc1c3dc293
Walaa <walaa@yamsafer.me>
2016-12-26 11:50:54 +0200

    Last modification according to select bugs

--- build/angular-bootstrap-select.js
+++ build/angular-bootstrap-select.js
@@ -2,13 +2,13 @@
 angular.module('angular-bootstrap-select.extra', [])
   .directive('toggle', function () {
     return {
-      restrict: 'A',
+      restrict: 'ACE',
       link: function (scope, element, attrs) {
         // prevent directive from attaching itself to everything that defines a toggle attribute
-        if (!element.hasClass('selectpicker')) {
+        if (!element.hasClass('selectpicker') && !attrs.selectpicker && element[0].localName !== 'selectpicker')) {
           return;
         }
-        var element = $('.selectpicker');
+
         var target = element.parent();
         var toggleFn = function () {
           target.toggleClass('open');
@@ -35,9 +35,18 @@ angular.module('angular-bootstrap-select', [])
       require: '?ngModel',
       priority: 10,
       compile: function (tElement, tAttrs, transclude) {
+        tElement.selectpicker($parse(tAttrs.selectpicker)());
+        tElement.selectpicker('refresh');
+        
         return function (scope, element, attrs, ngModel) {
-          var element = $('.selectpicker');
           if (!ngModel) return;
+
+          if (attrs.ngDisabled) {
+            scope.$watch(attrs.ngDisabled, function (newVal, oldVal) {
+              element.prop('disabled', newVal);
+              element.selectpicker('refresh');
+            });
+          }
 
           scope.$watch(attrs.ngModel, function (newVal, oldVal) {
             scope.$evalAsync(function () {
@@ -48,11 +57,11 @@ angular.module('angular-bootstrap-select', [])
 
           ngModel.$render = function () {
             scope.$evalAsync(function () {
-               if (element.hasClass('.bs-select-hidden')) element.selectpicker('refresh');
-               else element.selectpicker($parse(attrs.selectpicker));
+              element.selectpicker('refresh');
             });
           }
         };
       }
+
     };
   }]);
